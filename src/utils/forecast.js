b30e4fee5e3f252a61dcc6b01fe94cd2e7ccbc93
kmchen <kmchen@umich.edu>
2022-03-23 17:23:05 -0400

    added the humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,7 @@ const forecast = (latitude, longitude, callback) => {
         else {
             // const data = JSON.parse(response.body)
             // request option parses json for us
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
